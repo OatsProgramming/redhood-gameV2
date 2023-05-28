e35126576c52c49d8e3d78cc15e80723331810af
Jack Oats <jackoatmeals@gmail.com>
2023-05-28 18:33:40 -0500

    update: title change

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import localFont from 'next/font/local'
 const retroPixel = localFont({ src: '../assets/fonts/retro-pixel-thick.woff2' });
 
 export const metadata = {
-  title: "Paper Pixel Planet",
-  description: 'Save a Universe of Imagination: Immerse yourself in a captivating narrative that celebrates the power of creativity and imagination. Join forces with quirky characters, face the challenges of a troubled world, and ignite the spark of hope to save the Paper Pixel Planet from impending darkness.',
+  title: "Pixel Planet",
+  description: 'Save a Universe of Imagination: Immerse yourself in a captivating narrative that celebrates the power of creativity and imagination. Join forces with quirky characters, face the challenges of a troubled world, and ignite the spark of hope to save the Pixel Planet from impending darkness.',
 }
 
 export default function RootLayout({
