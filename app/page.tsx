54120b45299fea1176384439d4fdcb58d373972d
Jack Oats <jackoatmeals@gmail.com>
2023-05-29 12:54:02 -0500

    update: map layout of initial spawn

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,8 @@ import meats from "@/lib/inGameObjs/ingestible/meats";
 import pastries from "@/lib/inGameObjs/ingestible/pastries";
 import veggies from "@/lib/inGameObjs/ingestible/veggies";
 
+import styles from './page.module.css'
+
 const InventoryDialog = lazy(() =>
   import('./components/Dialog/InventoryDialog/InventoryDialog')
 )
@@ -28,87 +30,117 @@ export default function App() {
     <>
       <InteractBtns />
       <Character />
-      <Obstacle
-        isInteractive
-        items={beverages}
-        image={obstacleUrls.woodenCart}
-        style={{
-          top: '70vh',
-          left: '20vw',
-          scale: '1.5'
-        }}
-      />
-      <Obstacle
-        isInteractive
-        items={fruits}
-        image={obstacleUrls.fruitStall}
-        style={{
-          top: '70vh',
-          left: '40vw',
-          scale: '2.5'
-        }}
-      />
-      <Obstacle
-        isInteractive
-        items={meats}
-        image={obstacleUrls.meatStall}
-        style={{
-          top: '70vh',
-          left: '60vw',
-          scale: '2.5'
-        }}
-      />
-      <Obstacle
-        isInteractive
-        items={pastries}
-        image={obstacleUrls.pastryStall}
-        style={{
-          top: '70vh',
-          left: '80vw',
-          scale: '2'
-        }}
-      />
-      <Obstacle
-        isInteractive
-        items={seafood}
-        image={obstacleUrls.seafoodStall}
-        style={{
-          top: '80vh',
-          left: '10vw',
-          scale: '3'
-        }}
-      />
-       <Obstacle
-        isInteractive
-        items={veggies}
-        image={obstacleUrls.veggieStall}
-        style={{
-          top: '80vh',
-          left: '30vw',
-          scale: '1'
-        }}
-      />
-      <Obstacle
-        isInteractive
-        items={equipments}
-        image={obstacleUrls.woodenCart}
-        style={{
-          top: '30vh',
-          left: '30vw',
-          scale: '1.5'
-        }}
-      />
-      <Obstacle
-        isInteractive
-        items={potions}
-        image={obstacleUrls.purpleStall}
-        style={{
-          top: '40vh',
-          left: '10vw',
-          scale: '2'
-        }}
-      />
-      <Obstacle
+      <section className={styles['mapLayout']}>
+        <div className={styles['greenery']}>
+          <Obstacle
+            image={obstacleUrls.tree}
+            style={{
+              top: '',
+              left: '',
+              scale: '4',
+            }}
+            hitBoxStyle={{
+              width: 30,
+              left: '-3px'
+            }}
+          />
+          <Obstacle
+            image={obstacleUrls.bush}
+            style={{
+              top: '100%',
+              left: '0',
+              scale: '1'
+            }}
+          />
+        </div>
+        <div className={styles['stallOne']}>
+          <Obstacle
+            isInteractive
+            items={meats}
+            image={obstacleUrls.meatStall}
+            style={{
+              top: '80%',
+              left: '25%',
+              scale: '2.5'
+            }}
+          />
+          <Obstacle
+            isInteractive
+            items={pastries}
+            image={obstacleUrls.pastryStall}
+            style={{
+              top: '25%',
+              left: '20%',
+              scale: '2'
+            }}
+          />
+          <Obstacle
+            isInteractive
+            items={beverages}
+            image={obstacleUrls.woodenCart}
+            style={{
+              top: '40%',
+              left: '',
+              scale: '1.5'
+            }}
+          />
+        </div>
+        <div className={styles['stallTwo']}>
+          <Obstacle
+            isInteractive
+            items={equipments}
+            image={obstacleUrls.woodenCart}
+            style={{
+              top: '0',
+              left: '',
+              scale: '1.5'
+            }}
+          />
+          <Obstacle
+            isInteractive
+            items={fruits}
+            image={obstacleUrls.fruitStall}
+            style={{
+              top: '70%',
+              left: '70%',
+              scale: '2.5'
+            }}
+          />
+          <Obstacle
+            isInteractive
+            items={veggies}
+            image={obstacleUrls.veggieStall}
+            style={{
+              top: '60%',
+              left: '10%',
+              scale: '1.5'
+            }}
+          />
+        </div>
+        <div className={styles['stallThree']}>
+          <Obstacle
+            isInteractive
+            items={seafood}
+            image={obstacleUrls.seafoodStall}
+            style={{
+              top: '40%',
+              left: '',
+              scale: '3'
+            }}
+          />
+          <Obstacle
+            isInteractive
+            items={potions}
+            image={obstacleUrls.purpleStall}
+            style={{
+              top: '100%',
+              left: '',
+              scale: '2'
+            }}
+          />
+        </div>
+      </section>
+      {/* <Obstacle
         image={obstacleUrls.bush}
         style={{
           top: '60vh',
@@ -123,20 +155,7 @@ export default function App() {
           left: '80vw',
           scale: '1'
         }}
-      />
-      <Obstacle
-        image={obstacleUrls.tree}
-        style={{
-          top: '35vh',
-          left: '50vw',
-          scale: '3',
-        }}
-        hitBoxStyle={{
-          width: 30,
-          left: '-3px'
-        }}
-      />
-
+      /> */}
       <InventoryDialog />
       <StatsDisplay />
     </>
